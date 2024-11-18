76f5f6efbbdbb54433d6d3331e1433f5c13a47ea
Katri <katri.laamala@outlook.com>
2024-11-18 09:06:58 +0200

    fix

--- src/tests/validate_reference_test.py
+++ src/tests/validate_reference_test.py
@@ -10,13 +10,11 @@ class TestReferenceValidation(unittest.TestCase):
         validate_book("Test Author", "Test Title", "2001", "Test Publisher")
 
 
-    def test_too_short_or_unproper_year_raises_error(self):
+    def test_too_short_or_invalid_year_raises_error(self):
         with self.assertRaises(UserInputError):
             validate_book("Test Author", "Test Title", "200", "Test Publisher")
 
         with self.assertRaises(UserInputError):
             validate_book("Test Author", "Test Title", "2oo1", "Test Publisher")
 
-    def test_missing_fields_raises_error(self):
-        with self.assertRaises(UserInputError):
-            validate_book("Test Author", "Test Title", "2001")
+

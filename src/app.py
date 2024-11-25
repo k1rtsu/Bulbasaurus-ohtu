501e016c6c44292612fda9db85d00ca71590f299
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-25 12:02:16 +0200

    Handling form submissions on single page route

--- src/app.py
+++ src/app.py
@@ -14,11 +14,27 @@ def index():
 
 @app.route("/single_page_app", methods=['GET', 'POST'])
 def single_page_app(error = None):
+    if request.method == 'POST':
+        try:
+            reference_type = request.form.get('submit')
+            if reference_type == "inproceedings":
+                handle_add_inproceeding(request.form)
+            elif reference_type == 'book':
+                handle_add_book(request.form)
+            elif reference_type == 'misc':
+                handle_add_misc(request.form)
+            elif reference_type == 'article':
+                handle_add_article(request.form)
+        except Exception as e:
+            error = e
+
     books = refs.get_all_books()
     articles = refs.get_all_articles()
-    total = len(books)+len(articles)
+    miscs = refs.get_all_misc()
+    inproceedings = refs.get_all_inproceedings()
+    total = len(books)+len(articles)+len(miscs)+len(inproceedings)
 
-    return render_template("single_page_app.html", error = error, books=books, articles=articles, total=total)
+    return render_template("single_page_app.html", error=error, books=books, articles=articles, miscs=miscs, inproceedings=inproceedings, total=total)
 
 @app.route("/new_book_reference")
 def new_book(error = None):
@@ -35,6 +51,70 @@ def new_misc(error=None):
 @app.route("/new_inproceedings_reference")
 def new_inproceedings(error = None):
     return render_template("new_inproceedings_reference.html", error = error)
+
+def handle_add_book(form):
+    author = request.form["author"]
+    title = request.form["title"]
+    year = request.form["year"]
+    publisher = request.form["publisher"]
+    try:
+        validate_book(author, title, year, publisher)
+        refs.add_book(author, title, year, publisher)
+    except Exception as error: #pylint: disable=broad-exception-caught
+        raise error
+        
+def handle_add_misc(form):
+    author = request.form["author"]
+    title = request.form["title"]
+    year = request.form["year"]
+    note = request.form["note"]
+    try:
+        validate_misc(author, title, year, note)
+        refs.add_book(author, title, year, note)
+        return redirect("/references")
+    except Exception as error: #pylint: disable=broad-exception-caught
+        return new_misc(error)
+        
+def handle_add_article(form):
+    author = request.form["author"]
+    title = request.form["title"]
+    journal = request.form["journal"]
+    volume = request.form["volume"]
+    number = request.form["number"]
+    year = request.form["year"]
+    pages_from = request.form["pages_from"]
+    pages_to = request.form["pages_to"]
+    doi = request.form["doi"]
+    url = request.form["url"]
+    article_data = {
+            "author": author,
+            "title": title,
+            "journal": journal,
+            "volume": volume,
+            "number": number,
+            "year": year,
+            "pages_from": pages_from,
+            "pages_to": pages_to,
+            "doi": doi,
+            "url": url
+    }
+    try:
+        validate_article(article_data)
+        refs.add_article(article_data)
+    except Exception as error: #pylint: disable=broad-exception-caught
+        raise error
+        
+def handle_add_inproceeding(form):
+    if request.form.get("submit") == "inproceedings":
+        author = request.form["author"]
+        title = request.form["title"]
+        year = request.form["year"]
+        booktitle = request.form["booktitle"]
+        try:
+            validate_inproceedings(author, title, year, booktitle)
+            refs.add_inproceedings(author, title, year, booktitle)
+        except Exception as error: #pylint: disable=broad-exception-caught
+            raise error
 
 @app.route("/add_reference", methods=["POST"])
 def add(): #pylint: disable=inconsistent-return-statements

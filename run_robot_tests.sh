43f7d661297376698d07be4077278a18d921cd86
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-04 19:46:44 +0200

    Update run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -20,11 +20,7 @@ done
 echo "Flask server is ready"
 
 # suoritetaan testit
-poetry run robot --variable HEADLESS:true src/story_tests/article.robot
-poetry run robot --variable HEADLESS:true src/story_tests/references.robot
-poetry run robot --variable HEADLESS:true src/story_tests/search.robot
-poetry run robot --variable HEADLESS:true src/story_tests/home.robot
-poetry run robot --variable HEADLESS:true src/story_tests/book.robot
+poetry run robot --variable HEADLESS:true src/story_tests/
 
 status=$?
 

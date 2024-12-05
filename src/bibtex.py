7b36d59e1dd74f0159e33b150f475db545b4eea5
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-05 18:59:44 +0200

    Add a method in BibTex for sipmplifying data

--- src/bibtex.py
+++ src/bibtex.py
@@ -23,8 +23,30 @@ class BibTex:
         self.reference_info = reference_info
         self.reference_type = reference_info["type"]
         self.reference_id = reference_info["id"]
+        self.clean_reference_info_dictionary()
 
+    def clean_reference_info_dictionary(self):
+        """Make reference_info dictionary simpler by extractig only the values we need
+        for the BibTex references"""
+        new_dictionary = {}
+        for value in self.reference_info:
+            print(value)
+            if value not in ("id", "type"):
+                new_dictionary[value] = self.reference_info[value]["field"]
 
+        self.reference_info = new_dictionary
+
+    def type(self):
+        return self.reference_type
+
+    def author(self):
+        return self.reference_info["author"]
+
+    def title(self):
+        return self.reference_info["title"]
+
+    def year(self):
+        return self.reference_info["year"]
 
 class Article(BibTex):
     pass
